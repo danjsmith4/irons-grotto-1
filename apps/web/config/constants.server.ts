4038bcc51f8692b400dda663110d6dcee27e6e39
Joe McKie <4980618+joemckie@users.noreply.github.com>
2025-06-17 22:04:14 +0100

    chore: remove unused env vars

--- apps/web/config/constants.server.ts
+++ apps/web/config/constants.server.ts
@@ -2,16 +2,6 @@ import 'server-only';
 import { z } from 'zod';
 
 const ServerConfigSchema = z.object({
-  zeplo: z.object({
-    apiKey: z.string(),
-    url: z.string(),
-  }),
-  temple: z.object({
-    groupName: z.string(),
-    groupId: z.string(),
-    groupKey: z.string(),
-    privateGroup: z.string(),
-  }),
   redisUrl: z.string(),
   discord: z.object({
     token: z.string(),
@@ -21,16 +11,6 @@ const ServerConfigSchema = z.object({
 });
 
 export const serverConstants = ServerConfigSchema.parse({
-  zeplo: {
-    apiKey: process.env.ZEPLO_API_KEY,
-    url: process.env.ZEPLO_URL,
-  },
-  temple: {
-    groupName: process.env.TEMPLE_GROUP_NAME,
-    groupId: process.env.TEMPLE_GROUP_ID,
-    groupKey: process.env.TEMPLE_GROUP_KEY,
-    privateGroup: process.env.TEMPLE_PRIVATE_GROUP,
-  },
   redisUrl: process.env.KV_REST_API_URL,
   discord: {
     token: process.env.DISCORD_TOKEN,
