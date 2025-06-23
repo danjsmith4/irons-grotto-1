21d114e4beb68d89654ce340148a15ff4ed899c9
Joe McKie <4980618+joemckie@users.noreply.github.com>
2025-06-23 19:46:21 +0100

    fix: build

--- apps/web/app/rank-calculator/utils/get-points-remaining-label.ts
+++ apps/web/app/rank-calculator/utils/get-points-remaining-label.ts
@@ -2,14 +2,14 @@ import { formatNumber } from './format-number';
 
 export function getPointsRemainingLabel(
   pointsRemaining: number,
-  throttleReason?: 'items' | 'GM CAs' | null,
+  throttleReason?: 'items' | 'Master CAs' | null,
 ) {
   if (throttleReason === 'items') {
     return 'Items required!';
   }
 
-  if (throttleReason === 'GM CAs') {
-    return 'GM CAs required!';
+  if (throttleReason === 'Master CAs') {
+    return 'Master CAs required!';
   }
 
   return pointsRemaining ? `(${formatNumber(pointsRemaining)})` : 'Completed';
