173bde5cd28f03d5965b02d183f49b9ed8d75c73
Matthew Mulhall <mattlm0831@gmail.com>
2025-07-29 14:58:57 -0400

    Fix multiplier arg not existing

--- apps/web/app/api/check-auto-rank/route.ts
+++ apps/web/app/api/check-auto-rank/route.ts
@@ -104,6 +104,7 @@ export async function GET(request: NextRequest) {
         collectionLogTotal,
         clueScrollPoints,
         collectionLogBonusPoints,
+        1.0,
         scaling,
       );
     const { pointsAwarded: totalNotableItemsPoints } =
