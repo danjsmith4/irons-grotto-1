8af44b02e7fac5e8deba63ccc09b9f660b167961
Matthew Mulhall <mattlm0831@gmail.com>
2025-07-29 11:39:42 -0400

    Fix page.tsx

--- apps/web/app/rank-calculator/page.tsx
+++ apps/web/app/rank-calculator/page.tsx
@@ -1,12 +1,8 @@
 import { fetchPlayerAccounts } from './data-sources/fetch-player-accounts';
 import { PlayerList } from './player-list';
-import React from 'react';
+
 export default async function RankCalculatorPlayerList() {
   const accounts = await fetchPlayerAccounts();
 
-  return (
-    <>
-      <PlayerList accounts={accounts} />
-    </>
-  );
+  return <PlayerList accounts={accounts} />;
 }
